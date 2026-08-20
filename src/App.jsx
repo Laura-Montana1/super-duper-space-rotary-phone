ba1cd56d40839fcfa79c1ef7b249513b6213cf81
Laura-Montana1 <laurag-montanag@unilibre.edu.co>
2026-08-20 00:35:41 +0000

    Primeros pasos en react

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
 import './App.css';
 
-function App() {
+//Creando un nuevo componente, Persona
+function Person(props){
+  return(
+    <div className="person" style={{backgroundColor:props.color}}>
+      <h3>Nombre: {props.nombre}</h3>
+      <p>Edad:{props.edad}</p>
+      <p>Salario:{props.sueldo}</p>
+    </div>
+  )
+}
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h1>Hola Mundo</h1>
+      {/*Aqui agrego mi componenete*/}
+      <Person nombre="Laura" edad="98" sueldo="1" color="rgba(156, 134, 236, 0.27)"/>
+      <Person nombre="Laura" edad="98" sueldo="1" color ="rgba(99, 61, 236, 0.68)"/>
+      <Person nombre="Laura" edad="98" sueldo="1"color ="rgba(75, 30, 236, 0.84)"/>
     </div>
   );
 }
-
-export default App;
